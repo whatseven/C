4db05e850137b85d22d7c77115ef15fa5af3822d
cuirq3 <cuirq3@outlook.com>
2021-04-24 18:48:51 +0800

    collect data

--- src/city_data/city_data.cpp
+++ src/city_data/city_data.cpp
@@ -80,6 +80,7 @@
 //		<< "\n";
 //	outFile.close();
 //}
+int filename_num = 6;
 
 struct ImageCluster
 {
@@ -113,6 +114,8 @@ std::vector<ImageCluster> solveCluster(const cv::Mat& vSeg, const std::map<cv::V
 	std::vector<ImageCluster> result;
 	for (auto colorIter = currentColor.begin(); colorIter != currentColor.end(); colorIter++) {
 		ImageCluster cluster;
+		if (bbox[colorIter->second].first.size() < 80 * 80)
+			continue;
 		cluster.box = CGAL::Bbox_2(
 			*std::min_element(bbox[colorIter->second].first.begin(), bbox[colorIter->second].first.end()),
 			*std::min_element(bbox[colorIter->second].second.begin(), bbox[colorIter->second].second.end()),
@@ -183,11 +186,28 @@ void write_box_kitti(const std::vector<cv::RotatedRect>& v_boxes_3d, const std::
 	for (const auto& v_box_3d : v_boxes_3d)
 	{
 		int index = &v_box_3d - &v_boxes_3d[0];
-
-		f_out << (boost::format("Car 0 0 0 %d %d %d %d %f %f %f %f %f %f %f\n") %
+		/*float ray_angle = std::acos((Eigen::Vector2f(1, 0).dot(Eigen::Vector2f(v_box_3d.center.x, v_box_3d.center.y))) 
+			/ Eigen::Vector2f(v_box_3d.center.x, v_box_3d.center.y).norm());
+		if ((Eigen::Vector3f(1, 0, 0).cross(Eigen::Vector3f(v_box_3d.center.x, v_box_3d.center.y, 0))).z() < 0)
+			ray_angle *= -1;
+		float alpha_angle = (-v_box_3d.angle / 180.f * M_PI) - ray_angle;
+		bool isValid = alpha_angle > -M_PI && alpha_angle < M_PI;
+		while (!isValid)
+		{
+			if (alpha_angle < -M_PI)
+				alpha_angle += 2 * M_PI;
+			if (alpha_angle > M_PI)
+				alpha_angle -= 2 * M_PI;
+			isValid = alpha_angle > -M_PI && alpha_angle < M_PI;
+		}*/
+		float alpha_angle = std::atan2(v_box_3d.center.y, v_box_3d.center.x);
+		//float alpha_angle = (-v_box_3d.angle / 180.f * M_PI);
+		if (v_box_3d.center.y <= 0)
+			continue;
+		f_out << (boost::format("Car 0 0 %f %d %d %d %d %f %f %f %f %f %f %f\n") % alpha_angle %
 			v_boxes_2d[index].xmin() % v_boxes_2d[index].ymin() % v_boxes_2d[index].xmax() % v_boxes_2d[index].ymax() %
-			(v_zes[index].y() - v_zes[index].x()) % v_box_3d.size.height % v_box_3d.size.width %
-			 v_box_3d.center.x % ((v_zes[index].y() + v_zes[index].x()) / 2) % v_box_3d.center.y %
+			(v_zes[index].y() - v_zes[index].x()) % v_box_3d.size.width % v_box_3d.size.height  %
+			 v_box_3d.center.x % v_zes[index].y() % v_box_3d.center.y %
 			(-v_box_3d.angle / 180.f * M_PI)).str();
 	}
 	f_out.close();
@@ -237,8 +257,8 @@ void isInModel(Surface_mesh model, std::vector<Point_3> queryPoints, std::vector
 {
 	//std::vector<char> axis_seq = { 'z','y','x' };
 	std::vector<AABB_Point> vertices;
-	std::list<Triangle> faces;
-	for (face_descriptor face_id : model.faces())
+	std::list<Triangle> faces(1,Triangle(AABB_Point(-1,0,0), AABB_Point(1,0,0), AABB_Point(0,0,-1)));
+	/*for (face_descriptor face_id : model.faces())
 	{
 		halfedge_descriptor hi = model.halfedge(face_id);
 		for (halfedge_descriptor hf_id : model.halfedges_around_face(hi))
@@ -247,7 +267,7 @@ void isInModel(Surface_mesh model, std::vector<Point_3> queryPoints, std::vector
 			vertices.push_back(AABB_Point(model.point(vi).x(), model.point(vi).y(), model.point(vi).z()));
 		}
 		faces.push_back(Triangle(vertices[0], vertices[1], vertices[2]));
-	}
+	}*/
 
 	std::vector<std::list<Triangle>::iterator> deletedIt;
 	for (auto it = faces.begin(); it != faces.end(); ++it)
@@ -264,6 +284,7 @@ void isInModel(Surface_mesh model, std::vector<Point_3> queryPoints, std::vector
 	// Build tree
 	Tree_tri tree(faces.begin(), faces.end());
 	vector<vector<int>> pointData_vectors;
+	int totalLoop = 0;
 	for (auto axis : axis_seq)
 	{
 		vector<int> pointData;
@@ -274,22 +295,23 @@ void isInModel(Surface_mesh model, std::vector<Point_3> queryPoints, std::vector
 			AABB_Point temp1, temp2;
 			if (axis == 'z')
 			{
-				temp1 = AABB_Point(queryPoints[i][0], queryPoints[i][1], -1000);
-				temp2 = AABB_Point(queryPoints[i][0], queryPoints[i][1], 1000);
+				temp1 = AABB_Point(queryPoints[i][0], queryPoints[i][1], -1);
+				temp2 = AABB_Point(queryPoints[i][0], queryPoints[i][1], 1);
 			}
 			else if (axis == 'y') {
-				temp1 = AABB_Point(queryPoints[i][0], -1000, queryPoints[i][2]);
-				temp2 = AABB_Point(queryPoints[i][0], 1000, queryPoints[i][2]);
+				temp1 = AABB_Point(queryPoints[i][0], -1, queryPoints[i][2]);
+				temp2 = AABB_Point(queryPoints[i][0], 1, queryPoints[i][2]);
 			}
 			else {
-				temp1 = AABB_Point(-1000, queryPoints[i][1], queryPoints[i][2]);
-				temp2 = AABB_Point(1000, queryPoints[i][1], queryPoints[i][2]);
+				temp1 = AABB_Point(-1, queryPoints[i][1], queryPoints[i][2]);
+				temp2 = AABB_Point(1, queryPoints[i][1], queryPoints[i][2]);
 			}
 
 			Segment segment_query(temp1, temp2);
 			std::list<Segment_intersection> intersections;
 			tree.all_intersections(segment_query, std::back_inserter(intersections));
 			int loopNum = intersections.size();
+			totalLoop += loopNum;
 			for (int j = 0; j < loopNum; j++)
 			{
 				const AABB_Point* p = boost::get<AABB_Point>(&(intersections.front()->first));
@@ -309,7 +331,7 @@ void isInModel(Surface_mesh model, std::vector<Point_3> queryPoints, std::vector
 				double yMax = *max_element(yCoords.begin(), yCoords.end());
 				double yMin = *min_element(yCoords.begin(), yCoords.end());
 				if (yMax == yMin) {
-					yMin = 0;
+					yMax = 1;
 				}
 				double yPoint;
 				if (axis == 'z')
@@ -331,7 +353,7 @@ void isInModel(Surface_mesh model, std::vector<Point_3> queryPoints, std::vector
 
 	for (int i = 0; i < queryPoints.size(); i++)
 	{
-		double distance = CGAL::to_double(tree.squared_distance(AABB_Point(queryPoints[i][0], queryPoints[i][1], queryPoints[i][2])));
+		//double distance = CGAL::to_double(tree.squared_distance(AABB_Point(queryPoints[i][0], queryPoints[i][1], queryPoints[i][2])));
 		bool pointData = true;
 		for (auto item_pointData_axis : pointData_vectors)
 		{
@@ -339,7 +361,7 @@ void isInModel(Surface_mesh model, std::vector<Point_3> queryPoints, std::vector
 				pointData = false;
 		}
 		voxel.push_back(pointData);
-		sdf.push_back(distance);
+		//sdf.push_back(distance);
 	}
 }
 
@@ -499,7 +521,7 @@ int main(int argc, char* argv[])
 	{
 		// Calibration
 		{
-			std::ofstream f_calibration((output_root_path / "training" / "calib" / (std::to_string(cur_num)+".txt")).string());
+			std::ofstream f_calibration((output_root_path / "training" / "calib" / (std::string(filename_num - std::to_string(cur_num).length(), '0') + std::to_string(cur_num)+".txt")).string());
 			f_calibration << (boost::format("P2: %f %f %f %f %f %f %f %f %f %f %f %f\n") % 400 % 0 % 400 % 0 % 0 % 400 % 400 % 0 % 0 % 0 % 1 % 0).str();
 			f_calibration.close();
 		}
@@ -527,10 +549,12 @@ int main(int argc, char* argv[])
 		std::vector <CGAL::Bbox_2> boxes_2d;
 		std::vector <cv::RotatedRect> boxes_3d;
 		std::vector<Point_2> zses;
+		Surface_mesh camera_coords_wtho_pitch;
+
 		for (const auto& building : clusters)
 		{
-			if(building.name != "116")
-				continue;
+			//if(building.name != "116")
+			//	continue;
 			int index = &building - &clusters[0];
 			// Transform the point cloud and meshes from world to camera
 			Eigen::Matrix4f camera_matrix = item_pos_pack.camera_matrix.matrix();
@@ -539,7 +563,7 @@ int main(int argc, char* argv[])
 				camera_matrix(1, 0), camera_matrix(1, 1), camera_matrix(1, 2), camera_matrix(1, 3),
 				camera_matrix(2, 0), camera_matrix(2, 1), camera_matrix(2, 2), camera_matrix(2, 3)
 			);
-			Surface_mesh item_mesh( meshes.at(building.name));
+			Surface_mesh item_mesh(meshes.at(building.name));
 			CGAL::Polygon_mesh_processing::transform(transform_to_camera, item_mesh);
 			
 			Point_set item_points_camera_coordinates;
@@ -566,10 +590,82 @@ int main(int argc, char* argv[])
 			boxes_3d.push_back(box.first);
 			zses.push_back(box.second);
 			boxes_2d.push_back(building.box);
-		}
-		write_box_kitti(boxes_3d, zses, boxes_2d,(root_current_frame / "label_2" / (std::to_string(cur_num)+".txt")).string());
-		//write_box_test(boxes_3d, zses, boxes_2d,(root_current_frame / "label_2" / (std::to_string(cur_num)+".xyz")).string());
 
+			/*
+			* TODO Collect voxel, point cloud in camera space(visible and invisible) and sdf value
+			*/
+			CGAL::Polygon_mesh_processing::transform(transform_back_to_xz_plane, item_mesh);
+			//test
+			Point_set point_test;
+			std::copy(item_mesh.points().begin(), item_mesh.points().end(), point_test.point_back_inserter());
+			CGAL::write_ply_point_set(
+				std::ofstream((root_current_frame / "point_cloud_camera_coordinates" / (std::to_string(index) + "_after_transform_to_xz_mesh.ply")).string()),
+				point_test);
+
+			float zmax, zmin, xmax, xmin, ymax, ymin;
+			std::vector<float> x_vertices, y_vertices, z_vertices;
+			for (auto item_point : item_points_camera_coordinates.points())
+			{
+				x_vertices.push_back(item_point.x());
+				y_vertices.push_back(item_point.y());
+				z_vertices.push_back(item_point.z());
+			}
+			zmax = *(std::max_element(z_vertices.begin(), z_vertices.end()));
+			zmin = *(std::min_element(z_vertices.begin(), z_vertices.end()));
+			xmax = *(std::max_element(x_vertices.begin(), x_vertices.end()));
+			xmin = *(std::min_element(x_vertices.begin(), x_vertices.end()));
+			ymax = *(std::max_element(y_vertices.begin(), y_vertices.end()));
+			ymin = *(std::min_element(y_vertices.begin(), y_vertices.end()));
+
+			float scale = zmax - zmin;
+			if (xmax - xmin > scale)
+				scale = xmax - xmin;
+			if (ymax - ymin > scale)
+				scale = ymax - ymin;
+
+			CGAL::Aff_transformation_3<K> transform_to_reconstruct_coord(
+				1 / scale, 0, 0, (-(xmax + xmin) / 2) / scale,
+				0, 1 / scale, 0, (-(ymax + ymin) / 2) / scale,
+				0, 0, 1 / scale, (-zmin) / scale - 0.8
+			);
+			CGAL::Polygon_mesh_processing::transform(transform_to_reconstruct_coord, item_mesh);
+			//test
+			point_test.clear();
+			std::copy(item_mesh.points().begin(), item_mesh.points().end(), point_test.point_back_inserter());
+			CGAL::write_ply_point_set(
+				std::ofstream((root_current_frame / "point_cloud_camera_coordinates" / (std::to_string(index) + "_to_reconstruct_coord_mesh.ply")).string()),
+				point_test);
+
+			int resolution = 64;
+			std::vector<Point_3> query_points = {Point_3(0.5,0.5,-0.5)};
+			std::vector<bool> voxel;
+			std::vector<double> sdf;
+			std::vector<char> axis_seq = {'y'};
+
+			/*for (int i = 0; i <= resolution; i++)
+				for (int j = 0; j <= resolution; j++)
+					for (int k = 0; k <= resolution; k++)
+						query_points.push_back(Point_3(-1 + 2.0 / resolution * i, -1 + 2.0 / resolution * j, -1 + 2.0 / resolution * k));*/
+
+			//isInModel(item_mesh, query_points, voxel, sdf, axis_seq);
+
+			//Write voxel and sdf
+			checkFolder(root_current_frame / "voxel" / (std::string(filename_num - std::to_string(cur_num).length(), '0') + std::to_string(cur_num)));
+			checkFolder(root_current_frame / "sdf" / (std::string(filename_num - std::to_string(cur_num).length(), '0') + std::to_string(cur_num)));
+			std::fstream voxelFile((root_current_frame / "voxel" / (std::string(filename_num - std::to_string(cur_num).length(), '0') + std::to_string(cur_num)) / (building.name + ".txt")).string(), std::ios::out);
+			std::fstream sdfFile((root_current_frame / "sdf" / (std::string(filename_num - std::to_string(cur_num).length(), '0') + std::to_string(cur_num)) / (building.name + ".txt")).string(), std::ios::out);
+			voxelFile << std::to_string(xmax) << " " << std::to_string(xmin) << " " << std::to_string(ymax) << " " << std::to_string(ymin) << " " << std::to_string(zmax) << " " << std::to_string(zmin) << std::endl;
+			for (auto item_voxel : voxel)
+				voxelFile << std::to_string(item_voxel) << std::endl;
+			for (auto item_sdf : sdf)
+				sdfFile << std::to_string(item_sdf) << std::endl;
+			voxelFile.close();
+			sdfFile.close();
+		}
+		write_box_kitti(boxes_3d, zses, boxes_2d,(root_current_frame / "label_2" / (std::string(filename_num - std::to_string(cur_num).length(), '0') + std::to_string(cur_num)+".txt")).string());
+		//write_box_test(boxes_3d, zses, boxes_2d,(root_current_frame / "label_2" / (std::to_string(cur_num)+".xyz")).string());
+		if (boxes_3d.size() == 0)
+			continue;
 
 		/*
 		 * Collect point cloud
@@ -580,24 +676,24 @@ int main(int argc, char* argv[])
 			//(item_cluster.name+".obj")
 		}
 		
-		/*
-		* TODO Collect voxel, point cloud in camera space(visible and invisible) and sdf value
-		*/
 
 		/*
 		* Collect imgs
 		*/
+
 		cv::cvtColor(imgs.at("rgb"), imgs.at("rgb"), cv::COLOR_RGB2BGR);
 		cv::cvtColor(imgs.at("segmentation"), imgs.at("segmentation"), cv::COLOR_RGB2BGR);
 		//cv::imwrite((root_current_frame / "image_2" / (std::to_string(cur_num) + ".tiff")).string(), imgs.at("depth_planar"));
-		cv::imwrite((root_current_frame / "image_2" / (std::to_string(cur_num) + ".png")).string(), imgs.at("rgb"));
+		cv::imwrite((root_current_frame / "image_2" / (std::string(filename_num - std::to_string(cur_num).length(), '0') + std::to_string(cur_num) + ".png")).string(), imgs.at("rgb"));
 		//cv::imwrite((root_current_frame / "image_2" / (std::to_string(cur_num) + ".png")).string(), imgs.at("segmentation"));
 		
-		tqdm_bar.progress(&item_pos_pack -&place_to_be_travel[0], place_to_be_travel.size());
+		//tqdm_bar.progress(&item_pos_pack -&place_to_be_travel[0], place_to_be_travel.size());
 		cur_num += 1;
 
+		tqdm_bar.progress(cur_num, 100);
+		if (cur_num > 100)
+			break;
 	}
-
 
 	return 0;
 }
